56414032352155197e0e1b4489572e202bf50390
Felipe Arthur <felipe.rosa.arthur27@gmail.com>
2023-06-30 08:42:25 -0300

    Feat/add docker compose (#1)
    * feat: add docker compose
    
    * feat: working docker compose and Dockerfile

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,16 @@
 use actix_web::{guard, web, web::Data, App, HttpResponse, HttpServer, Responder};
 use async_graphql::{http::GraphiQLSource, EmptyMutation, EmptySubscription, Object, SimpleObject};
 use async_graphql_actix_web::{GraphQLRequest, GraphQLResponse};
+use dotenv::dotenv;
+use sqlx::postgres::{PgPool, PgPoolOptions};
+use std::env;
 
 type Schema = async_graphql::Schema<Query, EmptyMutation, EmptySubscription>;
+
+struct AppData {
+    schema: Schema,
+    db: PgPool,
+}
 
 #[derive(SimpleObject)]
 struct User {
@@ -23,8 +31,8 @@ impl Query {
     }
 }
 
-async fn handler(schema: web::Data<Schema>, req: GraphQLRequest) -> GraphQLResponse {
-    schema.execute(req.into_inner()).await.into()
+async fn handler(app_data: web::Data<AppData>, req: GraphQLRequest) -> GraphQLResponse {
+    app_data.schema.execute(req.into_inner()).await.into()
 }
 
 async fn playground() -> impl Responder {
@@ -33,19 +41,57 @@ async fn playground() -> impl Responder {
         .body(GraphiQLSource::build().endpoint("/graphql").finish())
 }
 
+fn build_db_url() -> String {
+    let postgres_user = env::var("POSTGRES_USER").expect("POSTGRES_USER not set");
+    let postgres_password = env::var("POSTGRES_PASSWORD").expect("POSTGRES_PASSWORD not set");
+    let postgres_db = env::var("POSTGRES_DB").expect("POSTGRES_DB not set");
+    let postgres_host = env::var("POSTGRES_HOST").expect("POSTGRES_HOST not set");
+
+    format!(
+        "postgres://{}:{}@{}/{}",
+        postgres_user, postgres_password, postgres_host, postgres_db
+    )
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    dotenv().ok();
     let schema = Schema::build(Query, EmptyMutation, EmptySubscription).finish();
+    let postgres_url = build_db_url();
+    let port = env::var("PORT")
+        .unwrap_or_else(|_| String::from("8080"))
+        .parse::<u16>()
+        .unwrap();
 
+    let pool = match PgPoolOptions::new()
+        .max_connections(5)
+        .connect(&postgres_url)
+        .await
+    {
+        Ok(pool) => {
+            println!("Connected to database at {}", postgres_url);
+            pool
+        }
+        Err(err) => {
+            println!(
+                "Failed to connect to database at {}: {:?}",
+                postgres_url, err
+            );
+            std::process::exit(1)
+        }
+    };
     println!("playground started at: http://localhost:8000/graphql");
 
     HttpServer::new(move || {
         App::new()
-            .app_data(Data::new(schema.clone()))
+            .app_data(Data::new(AppData {
+                schema: schema.clone(),
+                db: pool.clone(),
+            }))
             .service(web::resource("/graphql").guard(guard::Post()).to(handler))
             .service(web::resource("/graphql").guard(guard::Get()).to(playground))
     })
-    .bind(("0.0.0.0", 8000))?
+    .bind(("0.0.0.0", port))?
     .run()
     .await
 }
